01566e6433206aa859c53038950f6c0a64b3151a
Wesley Santos <33479424+dk4trin@users.noreply.github.com>
2024-01-25 19:45:30 -0300

    Create test3.json.php

---
+++ test3.json.php
@@ -0,0 +1,7 @@
+{
+    "url": "https://raw.githubusercontent.com/dk4trin/swagger-xss/main/test3.yaml.php",
+    "urls": [{
+        "url": "https://raw.githubusercontent.com/dk4trin/swagger-xss/main/test3.yaml.php",
+        "name": "foo"
+    }]
+}
